b3c197bd361eae4365b3bcb04e502dae44c7f84f
Islam Salah <islamsalah007@gmail.com>
2018-10-18 11:53:05 +0200

    Enable android multidex

---
+++ app/src/main/java/com/blink22/skeleton/SkeletonApplication.kt
@@ -0,0 +1,14 @@
+package com.blink22.skeleton
+
+import android.app.Application
+import android.support.multidex.MultiDexApplication
+
+/**
+ * Created by Islam Salah on 10/18/18.
+ *
+ * https://github.com/IslamSalah
+ * islamsalah007@gmail.com
+ */
+class SkeletonApplication : MultiDexApplication() {
+
+}
